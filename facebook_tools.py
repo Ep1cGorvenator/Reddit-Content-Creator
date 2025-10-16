db289f446dd3182d5dafc2f9098606e01ef504e9
Antman04777 <123802459+Antman04777@users.noreply.github.com>
2025-10-16 20:13:13 +0200

    facebook api updated

--- facebook_tools.py
+++ facebook_tools.py
@@ -1,9 +1,11 @@
-from pyfacebook import GraphAPI
+from facebook import GraphAPI
 from dotenv import load_dotenv
 from api import facebook_access_token
+import json
 
 load_dotenv()
 # Initialize the Facebook Graph API with the access token from environment variables
 api = GraphAPI(access_token=facebook_access_token)
+profile = api.get_object('me', fields='first_name,location,link,email')
 
-print(api.app_id)
+print(json.dumps(profile, indent=4))
