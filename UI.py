05b568c40cc84e07811bc797fd4d5329a0fff6d7
Jason <Jasonglow8@gmail.com>
2025-10-18 17:28:14 +0200

    changed ui wide

--- UI.py
+++ UI.py
@@ -29,7 +29,7 @@ except ImportError:
 st.set_page_config(
     page_title="Gorilla Studios AI",
     page_icon="🦍",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="auto",
 )
 
